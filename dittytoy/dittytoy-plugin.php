c29fc6aafde5106a9b9c00ef3523ee16d7976a0d
Reinder Nijhoff <reinder@infi.nl>
2025-11-24 11:10:59 +0100

    Update dittytoy-plugin.php

--- dittytoy/dittytoy-plugin.php
+++ dittytoy/dittytoy-plugin.php
@@ -15,15 +15,10 @@ function dittytoy_install() {
 }
 
 function dittytoy_curl_get_contents($url) {
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	$data = curl_exec($ch);
-	curl_close($ch);
+    $response = wp_remote_get($url);
+    $body = wp_remote_retrieve_body($response);
 
-	return $data;
+    return $body;
 }
 
 function dittytoy_do_query($query, $timeout = 60*60) {
